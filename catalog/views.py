10dea90f333436dd269b3bc048faab24a1a981bb
mranjon007 <mithun.bishwash.cse@ulab.edu.bd>
2019-07-01 22:11:32 +0600

    booklist and book-detailadded

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 
 def index(request):
@@ -18,3 +19,13 @@ def index(request):
         'num_books_of_math_genre': num_books_of_math_genre,
     }
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
